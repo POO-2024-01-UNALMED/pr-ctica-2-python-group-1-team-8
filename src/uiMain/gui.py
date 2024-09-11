446ca1eebc2719fdc8008f4dca30bda35dfd43ef
Andrés Garcés <agarcesb@unal.edu.co>
2024-09-10 19:39:00 -0500

    Hasta aqui funciona

--- src/uiMain/gui.py
+++ src/uiMain/gui.py
@@ -42,20 +42,15 @@ class GUI:
         self.texto_saludo.grid(row=0, column=0, sticky='nsew', padx=8, pady=8)
         self.texto_saludo.config(state=tk.DISABLED)
 
-        def alternar_saludo():
-            self.texto_saludo.config(state=tk.NORMAL)
-            if self.texto_saludo.get(1.0, tk.END).strip() == self.__class__.saludo:
-                self.texto_saludo.delete(1.0, tk.END)
-                self.texto_saludo.insert(tk.END, self.__class__.descripcion)
-            else:
-                self.texto_saludo.delete(1.0, tk.END)
-                self.texto_saludo.insert(tk.END, self.__class__.saludo)
-            self.texto_saludo.config(state=tk.DISABLED)
-
         # Imagenes del local
 
+        self.canvas = tk.Canvas(self.frame, bg='white')
+        self.canvas.grid(row=1, column=0, sticky='nsew', padx=8, pady=8)
+        self.imagen = ImageTk.PhotoImage(Image.open('imagenes/tienda/tienda1.png'))
+        self.imagen_id = self.canvas.create_image(0, 0, anchor=tk.NW, image=self.imagen)
+
         # Botones TODO BORRAR
-        self.boton2 = tk.Button(self.frame, text="Boton 2", bg='black', fg='white', font=('Arial', 15, 'bold')).grid(row=1, column=0, sticky='nsew', padx=8, pady=8)
+        #self.boton2 = tk.Button(self.frame, text="Boton 2", bg='black', fg='white', font=('Arial', 15, 'bold')).grid(row=1, column=0, sticky='nsew', padx=8, pady=8)
 
         # ~~~~~~~~~~~~~~~~~~~~~~~~~~~
         # FRAME 2
@@ -74,58 +69,87 @@ class GUI:
         self.subframe1.rowconfigure((0,1), weight=1, uniform='bbb')
 
         # Mostrar integrantes y hoja de vida y cambiar con click en esta
-        def cambiar_integrante():
-            # Imagenes
-            paths1 = ['imagenes/integrantes/villa/images1.png', 'imagenes/integrantes/villa/images2.png', 'imagenes/integrantes/villa/images3.png', 'imagenes/integrantes/villa/images4.png']
-            paths2 = ['imagenes/integrantes/seba/images1.png', 'imagenes/integrantes/seba/images2.png', 'imagenes/integrantes/seba/images3.png', 'imagenes/integrantes/seba/images4.png']
-            paths3 = ['imagenes/integrantes/andres/images1.png', 'imagenes/integrantes/andres/images2.png', 'imagenes/integrantes/andres/images3.png', 'imagenes/integrantes/andres/images4.png']
-
-            paths = [paths1, paths2, paths3]
-
-            p = self.__class__.num_imagen_integrante
-
-            self.imagen1 = tk.PhotoImage(file=paths[p][0])
-            self.imagen1 = self.imagen1.subsample(4)
-            tk.Label(self.subframe1, image=self.imagen1).grid(row=0, column=0, sticky='nwse',
-                                                              padx=2, pady=2)
-            self.imagen2 = tk.PhotoImage(file=paths[p][1])
-            self.imagen2 = self.imagen2.subsample(4)
-            tk.Label(self.subframe1, image=self.imagen2).grid(row=0, column=1, sticky='nwse',
-                                                              padx=2, pady=2)
-            self.imagen3 = tk.PhotoImage(file=paths[p][2])
-            self.imagen3 = self.imagen3.subsample(4)
-            tk.Label(self.subframe1, image=self.imagen3).grid(row=1, column=0, sticky='nwse',
-                                                              padx=2, pady=2)
-            self.imagen4 = tk.PhotoImage(file=paths[p][3])
-            self.imagen4 = self.imagen4.subsample(4)
-            tk.Label(self.subframe1, image=self.imagen4).grid(row=1, column=1, sticky='nwse',
-                                                              padx=2, pady=2)
-
-            self.__class__.num_imagen_integrante += 1
-            if self.__class__.num_imagen_integrante > 2:
-                self.__class__.num_imagen_integrante = 0
-
-            # Hoja de vida TODO: Quitar texto placeholder
-            hv_villa = """Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquet, ex ac malesuada fringilla, tortor ante facilisis felis, a feugiat velit velit ac leo. Fusce in faucibus tellus. Proin quis enim eu diam faucibus tincidunt. Nulla efficitur maximus sapien. Curabitur faucibus eget arcu nec dictum. Mauris nec lacus auctor nunc ultrices egestas id at erat. Sed magna libero, accumsan eu ante ut, sagittis fermentum mauris. Nam tellus nibh, scelerisque quis posuere eget, condimentum sit amet sapien. Curabitur non luctus elit, sit amet semper magna. Pellentesque vel diam urna. Proin congue id lectus in venenatis. Sed eget vehicula urna, nec vehicula odio. Aliquam quis molestie purus. Ut non orci vitae ipsum facilisis congue ultricies non metus. """
-            hv_seba = """𓀔𓀇𓀅𓀋𓀡𓀡𓀕𓀠𓀧𓀨𓀣𓀷𓀷𓀿𓀿𓁀𓁶𓁰𓁴𓁿𓂀𓁾𓁵𓁯𓂞𓂤𓂗𓃃𓂾𓂺𓂹𓃞𓃙𓃖𓃓𓃕𓃓𓃜𓃘𓃙𓃟𓃛𓃞𓂺𓃂𓂿𓂺𓃃𓃂𓂛𓂏𓅱𓅥𓅩𓅦𓅹𓅸𓅳𓅩𓅪𓄭𓄫𓄮𓄬𓄗𓄑𓄌𓃦𓃧𓃨𓃤𓃟𓃓𓃅𓃁𓂽𓃂𓂊𓁾𓂀𓁽𓁼𓁠𓁛𓁟𓁦𓁜𓁭𓁡𓀔𓀇𓀅𓀋𓀡𓀡𓀕𓀠𓀧𓀨𓀣𓀷𓀷𓀿𓀿𓁀𓁶𓁰𓁴𓁿𓂀𓁾𓁵𓁯𓂞𓂤𓂗𓃃𓂾𓂺𓂹𓃞𓃙𓃖𓃓𓃕𓃓𓃜𓃘𓃙𓃟𓃛𓃞𓂺𓃂𓂿𓂺𓃃𓃂𓂛𓂏𓅱𓅥𓅩𓅦𓅹𓅸𓅳𓅩𓅪𓄭𓄫𓄮𓄬𓄗𓄑𓄌𓃦𓃧𓃨𓃤𓃟𓃓𓃅𓃁𓂽𓃂𓂊𓁾𓂀𓁽𓁼𓁠𓁛𓁟𓁦𓁜𓁭𓁡𓀔𓀇𓀅𓀋𓀡𓀡𓀕𓀠𓀧𓀨𓀣𓀷𓀷𓀿𓀿𓁀𓁶𓁰𓁴𓁿𓂀𓁾𓁵𓁯𓂞𓂤𓂗𓃃𓂾𓂺𓂹𓃞𓃙𓃖𓃓𓃕𓃓𓃜𓃘𓃙𓃟𓃛𓃞𓂺𓃂𓂿𓂺𓃃𓃂𓂛𓂏𓅱𓅥"""
-            hv_andres = """Creo en Dios Padre, Todopoderoso, Creador del cielo y de la tierra. Y en Jesucristo, su único Hijo, Nuestro Señor, que fue concebido por obra y gracia del Espíritu Santo, nació de Santa María Virgen, padeció bajo el poder de Poncio Pilato, fue crucificado, muerto y sepultado, descendió a los infiernos, al tercer día resucitó entre los muertos, subió a los cielos y está sentado a la derecha de Dios Padre, Todopoderoso."""
-            hojas_vida = [hv_villa, hv_seba, hv_andres]
-
-            # self.boton3 = (tk.Button(self.frame2, bg='black', command=cambiar_integrante, fg='white',font=('Arial', 15, 'bold'))
-            #                 .grid(row=0, column=0, sticky='nsew', padx=8, pady=8))
-            self.text = tk.Text(self.frame2, fg='black', font=('Arial', 9, 'bold'), wrap='word', cursor='hand2')
-            self.text.grid(row=0, column=0, sticky='nsew', padx=8, pady=8)
-            self.text.insert(tk.END, hojas_vida[p])
-            self.text.bind("<Button-1>", lambda event: cambiar_integrante())
-        cambiar_integrante() # Llamada inicial a la funcion
+        self.cambiar_integrante() # Llamada inicial a la funcion
 
         # Menubar
         menubar = tk.Menu(self.root)
         iniciomenu = tk.Menu(menubar, tearoff=0)
-        iniciomenu.add_command(label="Descripcion", command=alternar_saludo)
+        iniciomenu.add_command(label="Descripcion", command=lambda: self.alternar_saludo())
         iniciomenu.add_separator()
         iniciomenu.add_command(label="Salir", command=self.root.destroy)
         menubar.add_cascade(label="Inicio", menu=iniciomenu)
 
         self.root.config(menu=menubar)
+
+        self.root.bind("<Configure>", self.resizer)
         self.root.mainloop()
+
+    def resizer(self, event):
+        #global imagen_og, imagen_resizeada, imagen_nueva
+        # CANVAS 1 (izquierda)
+        width = self.canvas.winfo_width()
+        height = self.canvas.winfo_height()
+
+        self.imagen_og = Image.open('imagenes/tienda/tienda1.png')
+        self.imagen_resizeada = self.imagen_og.resize((width, height))
+        self.imagen_nueva = ImageTk.PhotoImage(self.imagen_resizeada)
+
+        self.canvas.itemconfig(self.imagen_id, image=self.imagen_nueva)
+
+        # CANVAS 2 (derecha)
+
+        width2 = self.subframe1.winfo_width()
+        height2 = self.subframe1.winfo_height()
+
+
+    def cambiar_integrante(self):
+        # Imagenes
+        paths1 = ['imagenes/integrantes/villa/images1.png', 'imagenes/integrantes/villa/images2.png',
+                  'imagenes/integrantes/villa/images3.png', 'imagenes/integrantes/villa/images4.png']
+        paths2 = ['imagenes/integrantes/seba/images1.png', 'imagenes/integrantes/seba/images2.png',
+                  'imagenes/integrantes/seba/images3.png', 'imagenes/integrantes/seba/images4.png']
+        paths3 = ['imagenes/integrantes/andres/images1.png', 'imagenes/integrantes/andres/images2.png',
+                  'imagenes/integrantes/andres/images3.png', 'imagenes/integrantes/andres/images4.png']
+
+        paths = [paths1, paths2, paths3]
+
+        p = self.__class__.num_imagen_integrante
+
+        self.imagen1 = tk.PhotoImage(file=paths[p][0])
+        tk.Label(self.subframe1, image=self.imagen1).grid(row=0, column=0, sticky='nwse',
+                                                          padx=2, pady=2)
+        self.image2 = tk.PhotoImage(file=paths[p][1])
+        tk.Label(self.subframe1, image=self.image2).grid(row=0, column=1, sticky='nwse',
+                                                          padx=2, pady=2)
+        self.image3 = tk.PhotoImage(file=paths[p][2])
+        tk.Label(self.subframe1, image=self.image3).grid(row=1, column=0, sticky='nwse',
+                                                          padx=2, pady=2)
+        self.image4 = tk.PhotoImage(file=paths[p][3])
+        tk.Label(self.subframe1, image=self.image4).grid(row=1, column=1, sticky='nwse',
+                                                          padx=2, pady=2)
+
+        self.__class__.num_imagen_integrante += 1
+        if self.__class__.num_imagen_integrante > 2:
+            self.__class__.num_imagen_integrante = 0
+
+        # Hoja de vida TODO: Quitar texto placeholder
+        hv_villa = """villavillavillavillavillavillavillavillavillavillavilla"""
+        hv_seba = """sebasebasebasebasebasebasebasebasebasebasebasebaseba"""
+        hv_andres = """aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"""
+        hojas_vida = [hv_villa, hv_seba, hv_andres]
+
+        self.text = tk.Text(self.frame2, fg='black', font=('Arial', 9, 'bold'), wrap='word', cursor='hand2')
+        self.text.grid(row=0, column=0, sticky='nsew', padx=8, pady=8)
+        self.text.insert(tk.END, hojas_vida[p])
+        self.text.bind("<Button-1>", lambda event: self.cambiar_integrante())
+
+
+    def alternar_saludo(self):
+        self.texto_saludo.config(state=tk.NORMAL)
+        if self.texto_saludo.get(1.0, tk.END).strip() == self.__class__.saludo:
+            self.texto_saludo.delete(1.0, tk.END)
+            self.texto_saludo.insert(tk.END, self.__class__.descripcion)
+        else:
+            self.texto_saludo.delete(1.0, tk.END)
+            self.texto_saludo.insert(tk.END, self.__class__.saludo)
+        self.texto_saludo.config(state=tk.DISABLED)
