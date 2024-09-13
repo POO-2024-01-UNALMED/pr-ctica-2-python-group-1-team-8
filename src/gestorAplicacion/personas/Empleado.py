a0d6ef6a631cfa8e28823e76d65a97308be636f2
Zusark <149200499+Zusark@users.noreply.github.com>
2024-09-13 00:00:13 -0500

    primer commit del día

--- src/gestorAplicacion/personas/Empleado.py
+++ src/gestorAplicacion/personas/Empleado.py
@@ -17,50 +17,50 @@ class Empleado(Persona):
     def ingresar_meta(self, meta):
         self._metas.append(meta)
     def ingresar_meta_alcanzada(self, meta):
-        self.metas_alcanzadas.append(meta)
+        self._metas_alcanzadas.append(meta)
     def ingresar_meta_caducada(self, meta):
-        self.metas_caducadas.append(meta)
+        self._metas_caducadas.append(meta)
     def ingresar_transaccion(self, transaccion):
-        self.transacciones.append(transaccion)
+        self._transacciones.append(transaccion)
 
     # Getters y setters
     def get_salario(self):
-        return self.salario
+        return self._salario
     def get_salario_porcentual(self):
-        return self.salario_porcentual
+        return self._salario_porcentual
     def get_acumulado_mensual(self):
-        return self.acumulado_mensual
+        return self._acumulado_mensual
     def get_dias_laborales(self):
-        return self.dias_laborales
+        return self._dias_laborales
     def get_tienda(self):
-        return self.tienda
+        return self._tienda
     def get_metas(self):
-        return self.metas
+        return self._metas
     def get_metas_alcanzadas(self):
-        return self.metas_alcanzadas
+        return self._metas_alcanzadas
     def get_metas_caducadas(self):
-        return self.metas_caducadas
+        return self._metas_caducadas
     def get_transacciones(self):
-        return self.transacciones
+        return self._transacciones
 
     def set_salario(self, salario):
-        self.salario = salario
+        self._salario = salario
     def set_salario_porcentual(self, salario_porcentual):
-        self.salario_porcentual = salario_porcentual
+        self._salario_porcentual = salario_porcentual
     def set_acumulado_mensual(self, acumulado_mensual):
-        self.acumulado_mensual = acumulado_mensual
+        self._acumulado_mensual = acumulado_mensual
     def set_dias_laborales(self, dias_laborales):
-        self.dias_laborales = dias_laborales
+        self._dias_laborales = dias_laborales
     def set_tienda(self, tienda):
-        self.tienda = tienda
+        self._tienda = tienda
     def set_metas(self, metas):
-        self.metas = metas
+        self._metas = metas
     def set_metas_alcanzadas(self, metas_alcanzadas):
-        self.metas_alcanzadas = metas_alcanzadas
+        self._metas_alcanzadas = metas_alcanzadas
     def set_metas_caducadas(self, metas_caducadas):
-        self.metas_caducadas = metas_caducadas
+        self._metas_caducadas = metas_caducadas
     def set_transacciones(self, transacciones):
-        self.transacciones = transacciones
+        self._transacciones = transacciones
 
     def metodo_abstracto(self):
         pass
